7d4866a357209f59566abec3222672039d15a289
StarKir <Vsadnikofhell@mail.ru>
2025-12-12 13:46:26 +0500

    tests for exam repo

--- tests/general_test.py
+++ tests/general_test.py
@@ -161,11 +161,25 @@ def pin_exam(exam_id: int, token):
         return None
     print("Pin successful")
 
+
+def check_pin(exam_id: int, token):
+    url = BASE_URL + f"/exams/{exam_id}/check_pinning"
+    headers = {"Authorization": f"Bearer {token}"}
+    payload = {"exam_id": exam_id}
+    try:
+        r = requests.get(url, json=payload, headers=headers)
+    except requests.RequestException as e:
+        print(f"Network error during pin exam: {e}")
+        return None
+    print("Check successful")
+
+
 if __name__ == "__main__":
     register_user(ADMIN_EMAIL, ADMIN_PASSWORD, "admin1")
     token = login_user(ADMIN_EMAIL, ADMIN_PASSWORD)
     exam_id = create_exam(token, title="Test Exam")
     pin_exam(exam_id, token)
+    check_pin(exam_id, token)
     card_id = add_card(exam_id, token)
     fill_card(exam_id, token, card_id)
     for i in range(10):
